f4a023443452746990e4371cdec74cf00289eafb
clytn579 <clytn579@gmail.com>
2021-11-11 12:24:26 -0800

    Final submission

--- tables.sql
+++ tables.sql
@@ -1,62 +1,54 @@
---Create Departments Table
-CREATE TABLE departments (
-	dept_no VARCHAR,
-	dept_name VARCHAR
-);
-
---Create dept_emp Table
-CREATE TABLE dept_emp (
-	emp_no INT,
-	dept_no VARCHAR
-);
-
---Create dept_manager Table
-CREATE TABLE dept_manager (
-	dept_no VARCHAR,
-	emp_no INT
-);
-
---Create employees Table
-CREATE TABLE employees (
-	emp_no INT,
-	emp_title_id VARCHAR,
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR,
-	hire_date DATE
-);
-
---Create salaries Table
-CREATE TABLE salaries (
-	emp_no INT,
-	salary MONEY
-);
-
---Create titles Table
+--Create titles table with emp_no as foreign (Do I need to add a primary key?)
 CREATE TABLE titles (
-	title_id VARCHAR,
-	title VARCHAR
+title_id VARCHAR (5) NOT NULL PRIMARY KEY,
+title VARCHAR (30) NOT NULL
 );
 
-SELECT * FROM employees;
+--Create employees table with emp_no primary key
+DROP TABLE employees;
+CREATE TABLE employees (
+emp_no INT NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR (30) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR (30) NOT NULL,
+	last_name VARCHAR (30) NOT NULL,
+	sex VARCHAR (1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+	);
+	
+--Create salaries table with emp_no as foreign
+CREATE TABLE salaries (
+emp_no INT NOT NULL,
+salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
 
---Join departments and dept_emp tables on dept_no
-SELECT departments.dept_name, dept_emp.emp_no, dept_emp.dept_no
-FROM departments
-INNER JOIN dept_emp ON
-departments.dept_no = dept_emp.dept_no;
+--Create departments table primary key dept_no
+DROP TABLE departments;
+CREATE TABLE departments (
+dept_no VARCHAR (5) NOT NULL PRIMARY KEY,
+dept_name VARCHAR (30) NOT NULL
+);
 
---Join departments and dept_manager on dept_no
-SELECT departments.dept_name, 
-FROM departments
-INNER JOIN dept_manager ON
+--Create dept_manager table with 2 foreign
+DROP TABLE dept_manager;
+CREATE TABLE dept_manager (
+dept_no VARCHAR (5) NOT NULL,
+emp_no INT NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+--Create debt_emp with emp_no and dept_no as foreign 
+DROP TABLE dept_emp;
+CREATE TABLE dept_emp (
+emp_no INT NOT NULL,
+dept_no VARCHAR (5) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
 
 
---Join departments and employees on emp_no
 
 
---Join departments and salaries on emp_no
-
-
---Join departments and titles on title_id and emp_title_id
